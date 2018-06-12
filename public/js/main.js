59903f177c581b2a427cae7fefb8c3a9c6ab3419
Mia Itri <Mitri011@gmail.com>
2018-06-11 23:29:32 -0700

    Full reversi board through Elapsed Time logic

--- public/js/main.js
+++ public/js/main.js
@@ -296,6 +296,7 @@ var old_board = [
 				  ];
 
 var my_color = ' ';
+var interval_timer;
 
 socket.on('game_update',function(payload){
 	console.log('*** Client Log Message : \'game_update\' \n\tpayload: '+JSON.stringify(payload));
@@ -328,6 +329,23 @@ socket.on('game_update',function(payload){
 	}
 
 	$('#my_color').html('<h3 id="my_color">I am '+my_color+'</h3');
+	$('#my_color').append('<h4>It is '+payload.game.whose_turn+'\'s turn. Elapsed time <span id="elapsed"></span></h4>');
+	clearInterval(interval_timer);
+	interval_timer = setInterval(function(last_time){
+		return function(){
+			//do the work of updating the ui
+			var d = new Date();
+			var elapsedmilli = d.getTime() -last_time;
+			var minutes = Math.floor((elapsedmilli / (60* 1000)));
+			var seconds = Math.floor((elapsedmilli % (60 * 1000))/1000);
+			if(seconds<10){
+				$('#elapsed').html(minutes+':0'+seconds);
+			}else{
+				$('#elapsed').html(minutes+':'+seconds);
+			}
+
+		}}(payload.game.last_move_time)
+		,1000);
 
 
 	/* animate changes to the board */
@@ -376,10 +394,14 @@ socket.on('game_update',function(payload){
 				else{
 					$('#'+row+'_'+column).html('<img src="assets/images/error_square.gif" alt="error square"/>');
 				}
+			}
 
-				/*set up interactivity*/
-				$('#'+row+'_'+column).off('click');
-				if(board[row][column] == ' '){
+			/*set up interactivity*/
+			$('#'+row+'_'+column).off('click');
+			$('#'+row+'_'+column).removeClass('hovered_over');
+
+			if(payload.game.whose_turn === my_color){
+				if(payload.game.legal_moves[row][column] === my_color.substring(0,1)){
 					$('#'+row+'_'+column).addClass('hovered_over');
 					$('#'+row+'_'+column).click(function(r,c){
 						return function(){
@@ -392,10 +414,6 @@ socket.on('game_update',function(payload){
 						};
 					}(row,column));
 				}
-				else{
-					$('#'+row+'_'+column).removeClass('hovered_over');
-				}
-
 			}
 		}
 	}
